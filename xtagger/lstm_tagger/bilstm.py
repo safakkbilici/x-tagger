d315cba474ce92206dd8eb533cbc5a93250d2c94
safak <safakk.bilici@gmail.com>
2021-05-17 20:57:40 +0300

    bidirectional lstm

---
+++ xtagger/lstm_tagger/bilstm.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class LSTMForPOSTagging(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim, n_layers, bidirectional, dropout, pad_idx):
+
+        super().__init__()
+
+        self.embedding = nn.Embedding(input_dim, embedding_dim, padding_idx = pad_idx)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers = n_layers,
+                            bidirectional = bidirectional, dropout = dropout if n_layers > 1 else 0)
+        self.fc = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+        embedded = self.dropout(self.embedding(text))
+        outputs, (hidden, cell) = self.lstm(embedded)
+        preds= self.fc(self.dropout(outputs))
+        return preds
