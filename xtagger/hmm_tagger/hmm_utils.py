41b092b9dd815da7ce7487cc5655e8e7101f28d0
safak <safakk.bilici@gmail.com>
2021-05-17 19:49:09 +0300

    dataset utils

--- xtagger/hmm_tagger/hmm_utils.py
+++ xtagger/hmm_tagger/hmm_utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from tqdm import tqdm
+import pickle
+
 def get_emission(word, tag, train_bag):
     tag_list = [pair for pair in train_bag if pair[1]==tag]
     count_tag = len(tag_list)
@@ -5,7 +9,6 @@ def get_emission(word, tag, train_bag):
     count_w_given_tag = len(w_given_tag_list)
     return (count_w_given_tag, count_tag)
 
-# Transition probability
 def get_transition(tag1, tag2, train_bag):
     tags = [pair[1] for pair in train_bag]
     count_tag1 = len([t for t in tags if t==tag1])
@@ -29,5 +32,17 @@ def get_transition_2(tag1, tag2, tag3, train_bag):
                 count_tag1_tag2_tag3 += 1
         except:
             pass
-
     return count_tag1_tag2_tag3, count_tag1_tag2
+
+def df_to_hmm_dataset(df):
+    data2list = []
+    for index, row in tqdm(df.iterrows(), total = df.shape[0]):
+        ner_tags = row["ner_tags"]
+        tokens = row["tokens"]
+        mapped = list(map(lambda x, y: (x,y), tokens, ner_tags))
+        data2list.append(mapped)
+    return data2list
+
+def save_as_pickle(hmm_dataset, name):
+    with open(f"{name}.pkl", wb) as handle:
+        pickle.dump(train_data2list, handle, protocol=pickle.HIGHEST_PROTOCOL)
