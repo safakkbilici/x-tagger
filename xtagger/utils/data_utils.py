5695ab32e2093a427efa92749aa020ed8e9f144e
safak <safakk.bilici@gmail.com>
2021-05-18 16:52:09 +0300

    huggingface datasets utils

--- xtagger/utils/data_utils.py
+++ xtagger/utils/data_utils.py
@@ -1,8 +1,11 @@
+import transformers
+import datasets as hfd
 import pickle
 import pandas as pd
 import torch
 from torchtext import data
 from torchtext import datasets
+
 
 def df_to_xtagger_dataset(df):
     data2list = []
@@ -89,4 +92,43 @@ def df_to_torchtext_data(df_train, df_test, device, batch_size, min_freq = 2,
     )
 
     return train_iterator, valid_iterator, test_iterator, TEXT, TAGS
+
+def tokenize_and_align_labels(examples, tokenizer, tags, label_all_tokens):
+    tokenized_inputs = tokenizer(examples["sentence"], truncation=True, is_split_into_words=True)
+    labels = []
+    for i, label in enumerate(examples[f"tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                idx = tags.index(label[word_idx])
+                label_ids.append(idx)
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                idx = tags.index(label[word_idx])
+                label_ids.append(idx if label_all_tokens else -100)
+            previous_word_idx = word_idx
+
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+def df_to_hf_dataset(df, tags, tokenizer, device, label_all_tokens=True):
+    dataset = hfd.Dataset.from_pandas(df)
+    dataset = dataset.map(tokenize_and_align_labels,
+                          fn_kwargs = {'tokenizer': tokenizer,'tags': tags, 'label_all_tokens': label_all_tokens},
+                          batched = True)
+    return dataset
+
+
+
+
 
