47ebdc068e8ee11d4effaa9f3b401d188f455aa3
safak <safakk.bilici@gmail.com>
2021-05-18 16:52:33 +0300

    init

--- xtagger/__init__.py
+++ xtagger/__init__.py
@@ -1,6 +1,10 @@
+import transformers
+import datasets
 from xtagger.hmm_tagger.hmm import HiddenMarkovModel
 from xtagger.lstm_tagger.lstm_tagger import LSTMForTagging
+from xtagger.bert_tagger.bert import BERTForTagging
 from xtagger.utils.data_utils import df_to_xtagger_dataset
 from xtagger.utils.data_utils import save_as_pickle
 from xtagger.utils.data_utils import xtagger_dataset_to_df
 from xtagger.utils.data_utils import df_to_torchtext_data
+from xtagger.utils.data_utils import df_to_hf_dataset
