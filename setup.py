cc9aa19e9b4ce555503aee7b1a0f13d9db9bc792
safak <safakk.bilici@gmail.com>
2021-05-20 11:13:59 +0300

    download url

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author = "Şafak Bilici",
     author_email = "safakk.bilici.2112@gmail.com",
     url = "https://github.com/safakkbilici/x-tagger",
+    download_url = "https://github.com/safakkbilici/x-tagger/archive/refs/tags/0.1.0.tar.gz",
     keywords = [
         "Hidden Markov Models",
         "Token Classification",
