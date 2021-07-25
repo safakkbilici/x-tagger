7389e57e0a7f2f8d38fca0943dabeb38b93acb62
safak <safakk.bilici@gmail.com>
2021-07-25 17:26:08 +0300

    user defined metrics

--- xtagger/__init__.py
+++ xtagger/__init__.py
@@ -10,6 +10,7 @@ from xtagger.utils.data_utils import df_to_torchtext_data
 from xtagger.utils.data_utils import df_to_hf_dataset
 from xtagger.utils.data_utils import text_to_xtagger_dataset
 from xtagger.utils.regex import EnglishRegExTagger
+from xtagger.utils.metrics import xMetrics
 
 
 
@@ -22,4 +23,9 @@ IMPLEMENTED_METRICS = [
     "avg_precision",
     "avg_recall",
     "acc",
+    "classwise_f1",
+    "classwise_precision",
+    "classwise_recall",
+    "report",
+    xMetrics
 ]
