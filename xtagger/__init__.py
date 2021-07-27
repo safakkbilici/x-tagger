b73bf13b1ac9958b2f9ef603b22710bd407c1816
safak <safakk.bilici@gmail.com>
2021-07-27 16:39:41 +0300

    monitoring metrics

--- xtagger/__init__.py
+++ xtagger/__init__.py
@@ -1,14 +1,18 @@
 from xtagger.hmm_tagger.hmm import HiddenMarkovModel
 from xtagger.lstm_tagger.lstm_tagger import LSTMForTagging
 from xtagger.bert_tagger.bert import BERTForTagging
-from xtagger.utils.data_utils import df_to_xtagger_dataset
-from xtagger.utils.data_utils import save_as_pickle
-from xtagger.utils.data_utils import xtagger_dataset_to_df
-from xtagger.utils.data_utils import df_to_torchtext_data
-from xtagger.utils.data_utils import df_to_hf_dataset
-from xtagger.utils.data_utils import text_to_xtagger_dataset
+from xtagger.utils.data_utils import (
+    df_to_xtagger_dataset,
+    save_as_pickle,
+    xtagger_dataset_to_df,
+    df_to_torchtext_data,
+    df_to_hf_dataset,
+    text_to_xtagger_dataset
+)
+    
 from xtagger.utils.regex import EnglishRegExTagger
 from xtagger.utils.metrics import xMetrics
+from xtagger.utils.callbacks import Checkpointing
 
 
 
@@ -25,5 +29,20 @@ IMPLEMENTED_METRICS = [
     "classwise_precision",
     "classwise_recall",
     "report",
+    "eval_loss",
+    "train_loss",
     xMetrics
 ]
+
+MONITOR = [
+    "train_loss",
+    "train_acc",
+    "train_avg_f1",
+    "train_avg_precision",
+    "train_avg_recall",
+    "eval_loss",
+    "eval_acc",
+    "eval_avg_f1",
+    "eval_avg_precision",
+    "eval_avg_recall",
+    ]
