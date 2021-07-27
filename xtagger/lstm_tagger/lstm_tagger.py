5b125a61a7ab7a1fded78a718e4a351480a348e0
safak <safakk.bilici@gmail.com>
2021-07-27 16:39:51 +0300

    monitoring lstm

--- xtagger/lstm_tagger/lstm_tagger.py
+++ xtagger/lstm_tagger/lstm_tagger.py
@@ -10,21 +10,32 @@ from tqdm.auto import tqdm
 from xtagger.utils.time_utils import epoch_time
 from xtagger.lstm_tagger.lstm import LSTM
 from xtagger.utils import metrics
+from xtagger.utils import callbacks
 
 class LSTMForTagging(object):
-    def __init__(self, TEXT, TAGS, embedding_dim=100, hidden_dim=128,
-                 n_layers = 2, bidirectional=True, dropout=0.25, cuda=True):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 pad_idx,
+                 tag_pad_idx,
+                 embedding_dim=100,
+                 hidden_dim=128,
+                 n_layers = 2,
+                 bidirectional=True,
+                 dropout=0.25,
+                 cuda=True
+    ):
 
-        self.TEXT = TEXT
-        self.TAGS = TAGS
-        self.input_dim = len(self.TEXT.vocab)
+
+        self.input_dim = input_dim
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
-        self.output_dim = len(self.TAGS.vocab)
+        self.output_dim = output_dim
         self.n_layers = n_layers
         self.bidirectional = bidirectional
         self.dropout = dropout
-        self.pad_idx = self.TEXT.vocab.stoi[self.TEXT.pad_token]
+        self.pad_idx = pad_idx
+        self.TAG_PAD_IDX = tag_pad_idx
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if cuda and self.device.type=="cpu":
@@ -49,20 +60,23 @@ class LSTMForTagging(object):
         self.model.apply(self.init_weigths)
         self.model = self.model.to(self.device)
         print(f'The model has {self.count_parameters():,} trainable parameters')
-        self.TAG_PAD_IDX = self.TAGS.vocab.stoi[self.TAGS.pad_token]
+        #self.TAG_PAD_IDX = self.TAGS.vocab.stoi[self.TAGS.pad_token]
 
         self.criterion = nn.CrossEntropyLoss(ignore_index = self.TAG_PAD_IDX)
         self.criterion = self.criterion.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters())
 
 
-    def fit(self, train_set, test_set, epochs=10, save_name = "lstm_model.pt", eval_metrics = ["acc"], result_type = "%"):
+    def fit(self, train_set, test_set, epochs=10, save_name = "lstm_model.pt", eval_metrics = ["acc"], result_type = "%", checkpointing = None):
         self.train_set = train_set
         self.test_set = test_set
         self._metrics = eval_metrics
         self.result_type = result_type
 
         metrics.check_eval_metrics(self._metrics)
+
+        if checkpointing != None:
+            callbacks.check_monitor_eval_metrics(checkpointing.monitor, eval_metrics)
 
         total = len(train_set) * epochs
         with tqdm(total = total) as tt:
@@ -81,6 +95,8 @@ class LSTMForTagging(object):
                     tt.update()
                 print("Evaluating...")
                 results = self._eval()
+                if checkpointing != None:
+                    checkpointing.save_in(self.model, results)
                 print(results)
 
     def evaluate(self, test_set=None, eval_metrics = ["acc"], result_type = "%"):
@@ -179,7 +195,7 @@ class LSTMForTagging(object):
         train_preds_oh, train_gt_oh = metrics.tag2onehot(train_y_pred, train_y_true, self.TAGS.vocab.itos)
 
         results = {}
-        results["TEST"] = metrics.metric_results(
+        results["eval"] = metrics.metric_results(
             test_gt_oh,
             test_preds_oh,
             self._metrics,
@@ -187,7 +203,7 @@ class LSTMForTagging(object):
             self.TAGS.vocab.itos
         )
 
-        results["TRAIN"] = metrics.metric_results(
+        results["train"] = metrics.metric_results(
             train_gt_oh,
             train_preds_oh,
             self._metrics,
@@ -195,8 +211,31 @@ class LSTMForTagging(object):
             self.TAGS.vocab.itos
         )
 
-        results["TEST LOSS"] = eval_loss
-        results["TRAIN LOSS"] = train_loss
+        results["eval_loss"] = eval_loss
+        results["train_loss"] = train_loss
         return results
+
+    def predict(self, sentence):
+        self.model.eval()
+        if isinstance(sentence, str):
+            nlp = spacy.load("en_core_web_sm")
+            tokens = [token.text for token in nlp(sentence)]
+        else:
+            tokens = [token for token in sentence]
+
+        if self.TEXT.lower:
+            tokens = [t.lower() for t in tokens]
+
+        numericalized_tokens = [self.TEXT.vocab.stoi[t] for t in tokens]
+        unk_idx = self.TEXT.vocab.stoi[self.TEXT.unk_token]
+        unks = [t for t, n in zip(tokens, numericalized_tokens) if n == unk_idx]
+        token_tensor = torch.LongTensor(numericalized_tokens)
+        token_tensor = token_tensor.unsqueeze(0).to(self.device)
+        predictions = self.model(token_tensor).squeeze(0)
+        top_predictions = predictions.argmax(-1)
+        predicted_tags = [self.TAGS.vocab.itos[t.item()] for t in top_predictions]
+        return list(zip(tokens, predicted_tags)), unks
+    
+
 
     
