11cd00e355c43329682a2508099aa1b746aa8b8e
safak <safakk.bilici@gmail.com>
2021-07-29 11:45:17 +0300

    changed

---
+++ xtagger/bert_tagger/bert.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from transformers import AutoModel
+
+class BERT(nn.Module):
+    def __init__(self, bert_name, output_dim, dropout):
+        super(BERT, self).__init__()
+        self.bert_name = bert_name
+
+        self.bert = AutoModel.from_pretrained(self.bert_name)
+        embed_dim = self.bert.config.to_dict()["hidden_size"]
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+        self.fc_out = nn.Linear(embed_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        contextualized_repr = self.dropout(self.bert(x)["last_hidden_state"])
+        out = self.fc_out(contextualized_repr)
+        return out
+        
