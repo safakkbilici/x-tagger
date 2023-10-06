529117ba2d19aa9f5c75cd8853d2e701c764fca9
Şafak Bilici <safakk.bilici.2112@gmail.com>
2023-10-06 22:04:21 +0300

    add metric writer

--- xtagger/callbacks/metrics.py
+++ xtagger/callbacks/metrics.py
@@ -1,20 +1,12 @@
+import json
+import os
+
 from typing import List, Union, Dict
 
 import numpy as np
+import xtagger
 from xtagger.callbacks.metrics_ import *
-
-
-def convert_to_onehot(preds: List[str], ground_truth: List[str], tags: List[str]):
-    digit_pred = np.array([tags.index(tag) for tag in preds])
-    digit_gt = np.array([tags.index(tag) for tag in ground_truth])
-
-    preds_onehot = np.zeros((digit_pred.size, len(tags)))
-    gt_onehot = np.zeros((digit_gt.size, len(tags)))
-
-    preds_onehot[np.arange(digit_pred.size), digit_pred] = 1
-    gt_onehot[np.arange(digit_gt.size), digit_gt] = 1
-
-    return preds_onehot, gt_onehot
+from xtagger.utils.helpers import makepath
 
 
 def metric_results(
@@ -22,9 +14,16 @@ def metric_results(
     y_pred: Union[np.ndarray, List[List[int]]],
     eval_metrics: List[BaseMetric],
     tags: List[str],
-) -> Dict[str, Dict[str, float]]:
+) -> Dict[str, Dict[str, float] | float]:
     results = {}
 
     for metric in eval_metrics:
         results[metric.mode] = metric(y_true, y_pred, tags)()
     return results
+
+def write_results(results, output_dir):
+    if not os.path.isdir(makepath(output_dir, "eval")):
+        os.mkdir(makepath(output_dir, "eval"))
+    
+    with open(makepath(output_dir, "eval", "results.json"), "w", encoding="utf-8") as f:
+        json.dump(results, f, ensure_ascii=False, indent=xtagger.GLOBAL_INDENT)
