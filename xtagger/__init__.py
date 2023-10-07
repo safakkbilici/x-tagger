76a1a1df9e4d25fc1cd333398f714f63a8b65a57
Şafak Bilici <safakk.bilici.2112@gmail.com>
2023-10-07 15:01:47 +0300

    default pretokenizer added as global variable

--- xtagger/__init__.py
+++ xtagger/__init__.py
@@ -1,9 +1,14 @@
+from typing import Tuple, List
+
+
 __version__ = "0.1.5"
 __author__ = "Safak Bilici"
 __license__ = "MIT"
 
 DISABLE_PROGRESS_BAR = False
 GLOBAL_INDENT = 4
+DATASET_TYPE = List[List[Tuple[str, str]]]
+DEFAULT_PRETOKENIZER = lambda x: x.lower().split()
 
 from xtagger.callbacks.metrics_ import *
 from xtagger.callbacks.metrics_ import BaseMetric
