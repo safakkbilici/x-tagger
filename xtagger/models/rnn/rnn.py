69579cfd8880368e9dc7f6cbe00ed139c5cd9f00
Şafak Bilici <safakk.bilici.2112@gmail.com>
2023-10-07 15:02:20 +0300

    evaluate and predict methods are added

--- xtagger/models/rnn/rnn.py
+++ xtagger/models/rnn/rnn.py
@@ -13,6 +13,7 @@ from xtagger.callbacks.metrics import metric_results, write_results
 from xtagger.callbacks.metrics_ import Accuracy, BaseMetric
 from xtagger.tokenization.base import TokenizerBase
 from xtagger.utils.data import LabelEncoder, convert_to_dataloader
+from xtagger.utils.helpers import to_tensor, to_string
 
 
 class RNNTagger(nn.Module):
@@ -52,7 +53,7 @@ class RNNTagger(nn.Module):
 
     def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
         out = self.dropout(self.embedding(input_ids))
-        if self.rnn == "lstm":
+        if self.rnn == "LSTM":
             out, (hidden, cell) = self.model(out)
         else:
             out, hidden = self.model(out)
@@ -62,8 +63,8 @@ class RNNTagger(nn.Module):
 
     def fit(
         self,
-        train_set: Union[List[List[Tuple[str, str]]], DataLoader],
-        dev_set: Union[List[List[Tuple[str, str]]], DataLoader],
+        train_set: Union[xtagger.DATASET_TYPE, DataLoader],
+        dev_set: Union[xtagger.DATASET_TYPE, DataLoader],
         tokenizer: TokenizerBase,
         label_encoder: LabelEncoder,
         optimizer: torch.optim.Optimizer,
@@ -78,7 +79,7 @@ class RNNTagger(nn.Module):
         scheduler: Optional[torch.optim.lr_scheduler.LRScheduler] = None,
         max_length: Optional[int] = None,
         batch_size: Optional[int] = None,
-        pretokenizer: Callable = lambda x: x,
+        pretokenizer: Callable = xtagger.DEFAULT_PRETOKENIZER,
     ):
         self.to(device)
         if type(train_set) != DataLoader:
@@ -92,6 +93,9 @@ class RNNTagger(nn.Module):
                 shuffle=True,
             )
 
+        else:
+            train_dataloader = train_set
+
         if type(dev_set) != DataLoader:
             dev_dataloader = convert_to_dataloader(
                 dataset=train_set,
@@ -102,6 +106,9 @@ class RNNTagger(nn.Module):
                 pretokenizer=pretokenizer,
                 shuffle=False,
             )
+
+        else:
+            dev_dataloader = dev_set
 
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
@@ -239,15 +246,130 @@ class RNNTagger(nn.Module):
 
     def evaluate(
         self,
-        test_set: Union[List[List[Tuple[str, str]]], DataLoader],
+        test_set: Union[xtagger.DATASET_TYPE, DataLoader],
         tokenizer: TokenizerBase,
+        device: torch.device,
+        label_encoder: LabelEncoder,
+        batch_size: int,
+        max_length: int,
         criterion: Optional[nn.Module] = None,
+        eval_metrics: List[BaseMetric] = [Accuracy],
+        pretokenizer: Callable = xtagger.DEFAULT_PRETOKENIZER,
     ):
-        pass
+        self.to(device)
+        if type(test_set) != DataLoader:
+            test_dataloader = convert_to_dataloader(
+                dataset=test_set,
+                tokenizer=tokenizer,
+                label_encoder=label_encoder,
+                batch_size=batch_size,
+                max_length=max_length,
+                pretokenizer=pretokenizer,
+                shuffle=True,
+            )
+
+        else:
+            test_dataloader = test_set
+
+        results = defaultdict(lambda: defaultdict(dict))
+        y_true_test = []
+        y_pred_test = []
+        total_loss = 0
+        batch_count = 1
+
+        total = len(test_dataloader)
+        with tqdm(
+            total=total,
+            desc="Testing",
+            disable=xtagger.DISABLE_PROGRESS_BAR,
+            position=0,
+            leave=True,
+        ) as progressbar:
+            for idx, batch in enumerate(test_dataloader):
+                self.eval()
+
+                input_ids = batch["input_ids"].to(device)
+                labels = batch["labels"].to(device)
+
+                out = self.forward(input_ids=input_ids.long())
+                loss = criterion(out.permute(0, 2, 1).float(), labels.long())
+
+                preds = out.softmax(dim=-1).argmax(dim=-1).squeeze(dim=0).flatten()
+                ground_truth = labels.contiguous().view(-1)
+
+                non_pad_indices = (ground_truth != label_encoder.pad_tag_id).nonzero()
+                non_pad_preds = preds[non_pad_indices].squeeze(dim=-1).tolist()
+                non_pad_ground_truth = ground_truth[non_pad_indices].squeeze(dim=-1).tolist()
+
+                y_pred_test.extend([label_encoder[t] for t in non_pad_preds])
+                y_true_test.extend([label_encoder[t] for t in non_pad_ground_truth])
+
+                total_loss += loss.item()
+                batch_count += 1
+
+                progressbar.update()
+
+        test_results = metric_results(
+            y_pred=y_pred_test,
+            y_true=y_true_test,
+            eval_metrics=eval_metrics,
+            tags=label_encoder.maps,
+        )
+        total_loss = total_loss / batch_count
+
+        results["test"]["loss"] = total_loss
+        results["test"] = test_results
+
+        return results, total_loss
 
     @torch.inference_mode()
-    def predict(self, sentence: Union[str, List[str]], tokenizer: TokenizerBase):
-        pass
+    def predict(
+        self,
+        sentence: Union[str, List[str]],
+        tokenizer: TokenizerBase,
+        label_encoder: LabelEncoder,
+        device: torch.device,
+        max_length: Optional[int] = None,
+        pretokenizer: Callable = xtagger.DEFAULT_PRETOKENIZER
+    ):
+        self.eval()
+        if type(sentence) == str:
+            sentence = [sentence]
+
+        predictions = []
+        with tqdm(
+            total=len(sentence),
+            desc="Prediction",
+            disable=xtagger.DISABLE_PROGRESS_BAR,
+            position=0,
+            leave=True,
+        ) as progressbar:
+            for sequence in sentence:
+                encoded = tokenizer.encode(
+                    sentence=sequence, max_length=max_length, pretokenizer=pretokenizer
+                )
+
+                input_ids = to_tensor(encoded["input_ids"]).to(device)
+                out = self.forward(input_ids=input_ids.long())
+
+                preds = out.softmax(dim=-1).argmax(dim=-1).squeeze(dim=0).flatten().int().tolist()
+                preds = list(zip(input_ids.squeeze(dim=0).int().tolist(), preds))
+                preds = list(
+                    map(
+                        lambda x: (
+                            to_string(tokenizer.decode(x[0], remove_special_tokens=False)[0]),
+                            label_encoder[x[1]]
+                        ),
+                        preds,
+                    )
+                )
+                
+
+                predictions.append(preds)
+
+                progressbar.update()
+
+        return predictions
 
     def _autocast(self, dtype=torch.float16):
         class AMPContextManager:
@@ -263,6 +385,6 @@ class RNNTagger(nn.Module):
                     with autocast(dtype=self.dtype):
                         return func(*args, **kwargs)
 
-                return autocast
+                return autocast_decorator
 
         return AMPContextManager()
