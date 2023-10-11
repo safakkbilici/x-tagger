27b95a3d784850881fc6b437701df5e643bfc7a7
Şafak Bilici <safakk.bilici.2112@gmail.com>
2023-10-11 14:02:45 +0300

    default regex modes are added

--- xtagger/__init__.py
+++ xtagger/__init__.py
@@ -1,5 +1,4 @@
-from typing import Tuple, List
-
+from typing import List, Tuple
 
 __version__ = "0.1.5"
 __author__ = "Safak Bilici"
@@ -9,21 +8,31 @@ DISABLE_PROGRESS_BAR = False
 GLOBAL_INDENT = 4
 DATASET_TYPE = List[List[Tuple[str, str]]]
 DEFAULT_PRETOKENIZER = lambda x: x.lower().split()
+DEFAULT_REGEX_RULE = [
+    (r".*ing$", "VERB"),
+    (r".*ed$", "VERB"),
+    (r".*es$", "VERB"),
+    (r".*\'s$", "NOUN"),
+    (r".*s$", "NOUN"),
+    (r"\*T?\*?-[0-9]+$", "X"),
+    (r"^-?[0-9]+(.[0-9]+)?$", "NUM"),
+    (r".*", "NOUN"),
+]
 
 from xtagger.callbacks.metrics_ import *
 from xtagger.callbacks.metrics_ import BaseMetric
 from xtagger.models.hmm.hmm import HiddenMarkovModel
+from xtagger.models.regex.regex import RegexTagger
 from xtagger.models.rnn.rnn import RNNTagger
+from xtagger.tokenization.hf_interface import HFTokenizer
+from xtagger.tokenization.whitespace import WhiteSpaceTokenizer
 from xtagger.utils.data import (
+    LabelEncoder,
     Sampler,
     convert_from_dataframe,
     convert_from_file,
     convert_to_dataloader,
 )
-from xtagger.utils.regex import EnglishRegExTagger
-from xtagger.tokenization.whitespace import WhiteSpaceTokenizer
-from xtagger.tokenization.hf_interface import HFTokenizer
-from xtagger.utils.data import LabelEncoder
 
 IMPLEMENTED_METRICS = [
     F1,
@@ -35,8 +44,4 @@ IMPLEMENTED_METRICS = [
     ClasswiseRecall,
     ClassificationReport,
     BaseMetric,
-]
-
-IMPLEMENTED_REGEX_LANGUAGES = [
-    EnglishRegExTagger,
 ]
